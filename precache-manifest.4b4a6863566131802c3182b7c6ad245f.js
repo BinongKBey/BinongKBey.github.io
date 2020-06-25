d335e06e4427a31e0f5166c8631609e7a7668be2
BinongKBey <binongkbey1@gmail.com>
2020-06-25 21:01:08 +0530

    Updates

---
+++ precache-manifest.4b4a6863566131802c3182b7c6ad245f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b49804db7857fdebf0bcb21e54c720c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8935873e0482f0208814",
+    "url": "/static/css/main.d1012bf2.chunk.css"
+  },
+  {
+    "revision": "8894fbd64047e1d07148",
+    "url": "/static/js/2.6b1a177c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6b1a177c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8935873e0482f0208814",
+    "url": "/static/js/main.b0ae5de0.chunk.js"
+  },
+  {
+    "revision": "233dea908d4de0135036",
+    "url": "/static/js/runtime-main.c8273c4c.js"
+  },
+  {
+    "revision": "f3df789a31e3542a2d2bc5f0e4ceb207",
+    "url": "/static/media/project-1.f3df789a.png"
+  },
+  {
+    "revision": "a2c16e8cda37d08a08e920e46ada9049",
+    "url": "/static/media/project-2.a2c16e8c.jpg"
+  },
+  {
+    "revision": "bfa82ab6c718c599f8954b5688f176ce",
+    "url": "/static/media/project-3.bfa82ab6.jpg"
+  },
+  {
+    "revision": "44cf34aca46ff2ad72c746d28957a025",
+    "url": "/static/media/user.44cf34ac.png"
+  }
+]);
